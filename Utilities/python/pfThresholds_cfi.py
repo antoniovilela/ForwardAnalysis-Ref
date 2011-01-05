433fc102111a8d3dc87de033801429fdca7a3be6
antoniov <>
2011-01-05 10:24:26 +0000

    add PF-related cfgs

---
+++ Utilities/python/pfThresholds_cfi.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+pfThresholds = cms.PSet(
+    Barrel = cms.PSet(
+        hadronCharged = cms.PSet( pt = cms.double(0.5) ),
+        hadronNeutral = cms.PSet( energy = cms.double(1.4) ),
+        electron = cms.PSet( pt = cms.double(0.5) ),
+        gamma = cms.PSet( energy = cms.double(0.9) )
+    ),
+    Endcap = cms.PSet(
+        hadronCharged = cms.PSet( pt = cms.double(0.5) ),
+        hadronNeutral = cms.PSet( energy = cms.double(2.7) ),
+        electron = cms.PSet( pt = cms.double(0.5) ),
+        gamma = cms.PSet( energy = cms.double(2.5) )
+    ),
+    Transition = cms.PSet(
+        hadronCharged = cms.PSet( pt = cms.double(0.5) ),
+        hadronNeutral = cms.PSet( energy = cms.double(3.8) ),
+        electron = cms.PSet( pt = cms.double(0.5) ),
+        gamma = cms.PSet( energy = cms.double(2.5) ),
+        hadronHF = cms.PSet( energy = cms.double(4.0) ),
+        emHF = cms.PSet( energy = cms.double(3.5) )
+    ),
+    Forward = cms.PSet(
+        hadronHF = cms.PSet( energy = cms.double(4.0) ),
+        emHF = cms.PSet( energy = cms.double(3.5) )
+    )
+)
