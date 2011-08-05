ee10fccd96c4c6a6ae1c0517d083c7a4a800f3a0
antoniov <>
2011-08-05 15:37:18 +0000

    PF2PAT cfg

--- Skimming/test/patTuple_PF2PAT_data_cfg.py
+++ Skimming/test/patTuple_PF2PAT_data_cfg.py
@@ -1,5 +1,26 @@
 ## import skeleton process
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
+# ------------------------------------------------------
+#  In addition you usually want to change the following
+#  parameters:
+## ------------------------------------------------------
+#
+process.GlobalTag.globaltag =  'GR_R_42_V19::All'
+
+process.source.fileNames = [
+    'file:/storage2/eliza/samples_test/MultiJetPromptReco2_v4.root'
+]
+
+#                                         ##
+#   process.maxEvents.input = ...         ##  (e.g. -1 to run on all events)
+#                                         ##
+#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#                                         ##
+#   process.out.fileName = ...            ##  (e.g. 'myTuple.root')
+#                                         ##
+#   process.options.wantSummary = True    ##  (to suppress the long output at the end of the job)    
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
 # the source is already defined in patTemplate_cfg.
 # overriding source and various other things
@@ -7,7 +28,6 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 #process.source = cms.Source("PoolSource", 
 #     fileNames = cms.untracked.vstring('file:myAOD.root')
 #)
-
 
 # process.load("CommonTools.ParticleFlow.Sources.source_ZtoMus_DBS_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
@@ -18,7 +38,6 @@ process.out.fileName = cms.untracked.string('patTuple_PF2PAT.root')
 # load the PAT config
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-
 # Configure PAT to use PF2PAT instead of AOD sources
 # this function will modify the PAT sequences. It is currently 
 # not possible to run PF2PAT+PAT and standart PAT at the same time
@@ -27,21 +46,106 @@ from PhysicsTools.PatAlgos.tools.pfTools import *
 postfix = "PFlow"
 jetAlgo="AK5"
 usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=False, postfix=postfix) 
-# to run second PF2PAT+PAT with differnt postfix uncomment the following lines
-# and add it to path
-#postfix2 = "PFlow2"
-#jetAlgo2="AK7"
-#usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo2, runOnMC=True, postfix=postfix2) 
+#-----------------Customization----------------
+process.pfPileUpPFlow.Enable = True
+process.pfPileUpPFlow.checkClosestZVertex = False
+process.pfPileUpPFlow.Vertices = cms.InputTag('goodOfflinePrimaryVertices')
+process.pfPileUpPFlow.verbose = True
+process.pfJetsPFlow.doAreaFastjet = True
+process.pfJetsPFlow.doRhoFastjet = False
 
-# to use tau-cleaned jet collection uncomment the following:
-#getattr(process,"pfNoTau"+postfix).enable = True
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+process.goodOfflinePrimaryVertices = cms.EDFilter(
+    "PrimaryVertexObjectFilter",
+    filterParams = pvSelector.clone( minNdof = cms.double(4.0), maxZ = cms.double(24.0) ),
+    src=cms.InputTag('offlinePrimaryVertices')
+    )
 
-# to switch default tau to HPS tau uncomment the following:
-#adaptPFTaus(process,"hpsPFTau",postfix=postfix)
+# Compute the mean pt per unit area (rho) from the PFCHS inputs
+from RecoJets.Configuration.RecoPFJets_cff import kt6PFJets
+process.kt6PFJetsPFlow = kt6PFJets.clone(
+    src = cms.InputTag('pfNoElectron'+postfix),
+    doAreaFastjet = cms.bool(True),
+    doRhoFastjet = cms.bool(True)
+    )
+process.patJetCorrFactorsPFlow.rho = cms.InputTag("kt6PFJetsPFlow", "rho")
 
+"""
+process.pileUpN1PrimaryVertices = cms.EDFilter("RecoTauPileUpVertexSelector",
+    filter = cms.bool(False),
+    src = cms.InputTag('goodOfflinePrimaryVertices'),
+    minTrackSumPt = cms.double(0.)
+    #minTrackSumPt = cms.double(5.)
+    )
 
+process.pileUpN2PrimaryVertices = process.pileUpN1PrimaryVertices.clone(
+    src = cms.InputTag('pileUpN1PrimaryVertices')
+    )
+
+process.pileUpN3PrimaryVertices = process.pileUpN1PrimaryVertices.clone(
+    src = cms.InputTag('pileUpN2PrimaryVertices')
+    )
+
+process.pfPileUpN1 = process.pfPileUp.clone(
+    PFCandidates = cms.InputTag('pfPileUp'+postfix),
+    Vertices = cms.InputTag('pileUpN1PrimaryVertices'), 
+    verbose = True
+    )
+
+process.pfPileUpExclN1 = process.pfNoPileUp.clone(
+    name = cms.untracked.string('pileUpN1OnPFCandidates'),
+    bottomCollection = cms.InputTag('pfPileUp'+postfix),
+    topCollection = cms.InputTag('pfPileUpN1'),
+    verbose = True
+    )
+
+process.pfPileUpN2 = process.pfPileUp.clone(
+    PFCandidates = cms.InputTag('pfPileUpN1'),
+    Vertices = cms.InputTag('pileUpN2PrimaryVertices'),
+    verbose = True
+    )
+
+process.pfPileUpExclN2 = process.pfNoPileUp.clone(
+    name = cms.untracked.string('pileUpN2OnPFCandidates'),
+    bottomCollection = cms.InputTag('pfPileUpN1'),
+    topCollection = cms.InputTag('pfPileUpN2'),
+    verbose = True
+    )
+
+process.pfPileUpN3 = process.pfPileUp.clone(
+    PFCandidates = cms.InputTag('pfPileUpN2'),
+    Vertices = cms.InputTag('pileUpN3PrimaryVertices'),
+    verbose = True
+    )
+
+process.pfPileUpExclN3 = process.pfNoPileUp.clone(
+    name = cms.untracked.string('pileUpN3OnPFCandidates'),
+    bottomCollection = cms.InputTag('pfPileUpN2'),
+    topCollection = cms.InputTag('pfPileUpN3'),
+    verbose = True
+    )
+"""
+
+# Add modules to default sequence
+getattr(process,"patPF2PATSequence"+postfix).replace(
+    getattr(process,"pfNoElectron"+postfix),
+    getattr(process,"pfNoElectron"+postfix)*process.kt6PFJetsPFlow
+    )
+
+"""
+getattr(process,"patPF2PATSequence"+postfix).replace(
+    getattr(process,"pfNoPileUp"+postfix),
+    getattr(process,"pfNoPileUp"+postfix) * 
+    process.pfPileUpN1 * process.pfPileUpN2 * process.pfPileUpN3 *
+    process.pfPileUpExclN1 * process.pfPileUpExclN2 * process.pfPileUpExclN3
+    )
+"""
+
+#----------------------------------------------
 # Let it run
 process.p = cms.Path(
+    process.goodOfflinePrimaryVertices * 
+#    process.pileUpN1PrimaryVertices * process.pileUpN2PrimaryVertices * process.pileUpN3PrimaryVertices *
 #    process.patDefaultSequence  +
     getattr(process,"patPF2PATSequence"+postfix)
 #    second PF2PAT
@@ -55,7 +159,14 @@ from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
 process.out.outputCommands = cms.untracked.vstring('drop *',
                                                    'keep recoPFCandidates_particleFlow_*_*',
                                                    *patEventContentNoCleaning ) 
-
+# Adjust the event content
+process.out.outputCommands += [
+    #'keep *_selectedPat*_*_*',
+    #'keep double_*PFlow*_*_PAT',
+    'keep *_goodOfflinePrimaryVertices*_*_*',
+    #'keep *_pileUpN*PrimaryVertices_*_*',
+    #'keep *_pfPileUpExclN*_*_*'
+]
 
 # top projections in PF2PAT:
 getattr(process,"pfNoPileUp"+postfix).enable = True 
@@ -66,24 +177,3 @@ getattr(process,"pfNoJet"+postfix).enable = True
 
 # verbose flags for the PF2PAT modules
 getattr(process,"pfNoJet"+postfix).verbose = False
-
-## ------------------------------------------------------
-#  In addition you usually want to change the following
-#  parameters:
-## ------------------------------------------------------
-#
-process.GlobalTag.globaltag =  'GR_R_42_V19::All'    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
-#                                         ##
-process.source.fileNames = [          ##
-    'file:/storage2/eliza/samples_test/MultiJetPromptReco2_v4.root'
-   ]                                     ##  (e.g. 'file:AOD.root')
-#                                         ##
-#   process.maxEvents.input = ...         ##  (e.g. -1 to run on all events)
-#                                         ##
-#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
-#                                         ##
-#   process.out.fileName = ...            ##  (e.g. 'myTuple.root')
-#                                         ##
-#   process.options.wantSummary = True    ##  (to suppress the long output at the end of the job)    
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
