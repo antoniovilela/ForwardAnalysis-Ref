1f7f8d9fb7105fda040c7898e98cd435072d203f
antoniov <>
2011-08-12 11:57:21 +0000

    update config. files

--- Utilities/python/castorActivityFilter_cfi.py
+++ Utilities/python/castorActivityFilter_cfi.py
@@ -2,5 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 castorActivityFilter = cms.EDFilter("CastorActivityFilter",
     CastorRecHitTag = cms.InputTag("rechitcorrector"),
+    # MC
+    #CastorRecHitTag = cms.InputTag("castorreco"),
     SumEMaxCastor = cms.double(250.)
 )
