dfb115eb62b2dbe3dd8e887002abc345b9d2d29d
antoniov <>
2011-08-26 09:29:49 +0000

    remove python files

--- ForwardTTreeAnalysis/python/tracksOutsideJets_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-tracksOutsideJets = cms.EDProducer("TrackOutsideJetsSelector",
-   src = cms.InputTag("generalTracks"),
-   copyExtras = cms.untracked.bool(False),
-   copyTrajectories = cms.untracked.bool(False),
-   JetTag = cms.InputTag("leadingJets"),
-   JetConeSize = cms.double(0.5)
-)
