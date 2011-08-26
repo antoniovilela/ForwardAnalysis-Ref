5c960dfbb49c8c7d703025367db1ca760c527935
sfonseca <>
2011-08-26 18:28:37 +0000

    PF2PAT samples works:included pfpatcollections

--- AnalysisSequences/python/edmNtupleEtaMax_cfi.py
+++ AnalysisSequences/python/edmNtupleEtaMax_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from Utilities.AnalysisSequences.edmNtupleCandView_cfi import edmNtupleCandView as edmNtupleEtaMax
+from ForwardAnalysis.AnalysisSequences.edmNtupleCandView_cfi import edmNtupleCandView as edmNtupleEtaMax
 edmNtupleEtaMax.src = "etaMaxPFCands"
 edmNtupleEtaMax.variables = cms.VPSet( cms.PSet( tag = cms.untracked.string("etaMax"),
                                                  quantity = cms.untracked.string("eta") ) )
--- AnalysisSequences/python/edmNtupleEtaMin_cfi.py
+++ AnalysisSequences/python/edmNtupleEtaMin_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from Utilities.AnalysisSequences.edmNtupleCandView_cfi import edmNtupleCandView as edmNtupleEtaMin
+from ForwardAnalysis.AnalysisSequences.edmNtupleCandView_cfi import edmNtupleCandView as edmNtupleEtaMin
 edmNtupleEtaMin.src = "etaMinPFCands"
 edmNtupleEtaMin.variables = cms.VPSet( cms.PSet( tag = cms.untracked.string("etaMin"),
                                                  quantity = cms.untracked.string("eta") ) )
