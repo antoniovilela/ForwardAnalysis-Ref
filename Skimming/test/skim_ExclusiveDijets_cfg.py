c4afabd6a1152144da712514320d633c21d824f0
sfonseca <>
2011-08-28 11:14:56 +0000

    include trigger path for FWD

--- Skimming/test/skim_ExclusiveDijets_cfg.py
+++ Skimming/test/skim_ExclusiveDijets_cfg.py
@@ -6,7 +6,9 @@ from patTuple_PF2PAT_data_cfg import process
 import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 process.hltFilter = copy.deepcopy(hltHighLevel)
-process.hltFilter.HLTPaths = ['HLT_*']
+process.hltFilter.HLTPaths = ['HLT_ExclDiJet60_HFAND_v*', 
+                              'HLT_ExclDiJet60_HFOR_v*'
+                              ]
 
 # Add filter in paths
 prependPaths(process,'hltFilter') 
