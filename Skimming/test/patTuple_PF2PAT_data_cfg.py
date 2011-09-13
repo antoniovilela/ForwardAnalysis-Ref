66133adc5379f1314791a667a57ff13dc8872457
sfonseca <>
2011-09-13 17:10:46 +0000

    include PAT Trigger

--- Skimming/test/patTuple_PF2PAT_data_cfg.py
+++ Skimming/test/patTuple_PF2PAT_data_cfg.py
@@ -5,18 +5,18 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 #  parameters:
 ## ------------------------------------------------------
 #
-process.GlobalTag.globaltag =  'GR_R_42_V19::All'
+process.GlobalTag.globaltag =  'FT_R_42_V20A::All'
 
 process.source.fileNames = [
-   # 'file:/storage2/eliza/samples_test/MultiJetPromptReco2_v4.root'
-     'file:/storage2/eliza/JetMay10RecoRun2011.root'
+   'file:/storage2/eliza/samples_test/MultiJetPromptReco2_v4.root'
+#     'file:/storage2/eliza/JetMay10RecoRun2011.root'
+#'file:/storage2/eliza/samples_Apr21/JetB_Apr21_ReReco2011.root'
 ]
-
 process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.out.fileName = cms.untracked.string('patTuple_PF2PAT.root')
 
 # load the PAT config
@@ -61,6 +61,16 @@ process.kt6PFJetsPFlow = kt6PFJets.clone(
 process.patJetCorrFactorsPFlow.rho = cms.InputTag("kt6PFJetsPFlow", "rho")
 
 process.ak5PFJetsPileUp = ak5PFJets.clone( src = cms.InputTag('pfPileUp'+postfix) )
+
+
+# switch on PAT trigger
+# included by SFonseca
+from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
+#switchOnTrigger( process,triggerProducer = 'patTrigger', triggerEventProducer = 'patTriggerEvent', sequence = 'patPF2PATSequence',hltProcess = 'HLT', outputModule = 'out')
+switchOnTrigger( process ,sequence ='patPF2PATSequence'+postfix)
+process.patTrigger.addL1Algos = cms.bool( True )
+switchOnTrigger( process ,sequence = 'patPF2PATSequence'+postfix) # to fix event content
+#-------------------------------------------------------------------------------------------------------------
 
 """
 process.pileUpN1PrimaryVertices = cms.EDFilter("RecoTauPileUpVertexSelector",
@@ -149,14 +159,18 @@ process.p = cms.Path(
 #    + getattr(process,"patPF2PATSequence"+postfix2)
 )
 
+## Ref:http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/TopQuarkAnalysis/TopPairBSM/test/ttbsm_cfg.py?hideattic=0&revision=1.1.2.4&view=markup
+# included by SFonseca
+# Add PatTrigger output to the created file
+from PhysicsTools.PatAlgos.patEventContent_cff import patTriggerEventContent
+
 # Add PF2PAT output to the created file
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
-#process.load("CommonTools.ParticleFlow.PF2PAT_EventContent_cff")
-#process.out.outputCommands =  cms.untracked.vstring('drop *')
-process.out.outputCommands = cms.untracked.vstring('drop *',
+process.out.outputCommands += cms.untracked.vstring('drop *',
                                                    'keep recoPFCandidates_particleFlow_*_*',
-                                                   *patEventContentNoCleaning ) 
-# Adjust the event content
+                                                   *patEventContentNoCleaning) 
+
+ # Adjust the event content
 process.out.outputCommands += [
     #'keep *_selectedPat*_*_*',
     #'keep double_*PFlow*_*_PAT',
@@ -180,8 +194,15 @@ process.out.outputCommands += [
     'keep *_logErrorHarvester_*_*', 
     'keep *_castorreco_*_*',
  #------- Calo towers (just for now) ------ 
-    'keep *_towerMaker_*_*' 
+    'keep *_towerMaker_*_*',
+ #---------------PatTrigger----------------
+    'keep patTriggerObjects_patTrigger*_*_*',
+    'keep patTriggerFilters_patTrigger*_*_*',
+    'keep patTriggerPaths_patTrigger*_*_*',
+    'keep patTriggerEvent_patTriggerEvent*_*_*'
+
 ]
+
 
 # top projections in PF2PAT:
 getattr(process,"pfNoPileUp"+postfix).enable = True 
@@ -192,3 +213,4 @@ getattr(process,"pfNoJet"+postfix).enable = True
 
 # verbose flags for the PF2PAT modules
 getattr(process,"pfNoJet"+postfix).verbose = False
+
