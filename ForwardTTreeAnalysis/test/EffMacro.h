fee800ef5428a9d4bcf71ed4f89116b73d3de7c6
dmf <>
2012-03-22 16:10:42 +0000

    Efficiency Macro.

---
+++ ForwardTTreeAnalysis/test/EffMacro.h
@@ -0,0 +1,54 @@
+#ifndef EffMacro_h
+#define EffMacro_h
+
+#include <TFile.h>
+#include <TTree.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+class DiffractiveEvent;
+class ExclusiveDijetsEvent;
+class QCDEvent;
+
+class EffMacro {
+
+   TFile* inf;
+   TTree* tr;
+   TBranch *diff;
+   TBranch *excl;
+   TBranch *qcd;
+   DiffractiveEvent *eventdiff;
+   ExclusiveDijetsEvent *eventexcl;
+   QCDEvent *eventqcd;
+
+   std::string filein;
+   std::string fileinput;
+   std::string processname;
+   std::string processinput; 
+   std::string savehistofile;
+   double jet1PT;
+   double jet2PT;
+   int optnVertex;
+   int optTrigger;
+   bool switchWeightLumi;
+   bool switchWeightePw;
+   bool switchPreSel;
+   bool switchTrigger;
+   double weightlumipass;
+   int nbins;
+   std::vector<TH1D*> m_hVector_Evt_lumis;
+   std::vector<TH1D*> m_hVector_Eff_lumis;
+
+   public :
+   EffMacro() {}
+   ~EffMacro() { inf->Close(); }
+   
+   void Run(std::string, std::string, std::string, double, double, int, int, bool, bool, bool, bool, double, int);
+   void LoadFile(std::string,std::string);
+   void FillHistograms();
+
+};
+#endif
