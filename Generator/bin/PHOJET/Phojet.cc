e793581d57a3c0aa6a6a2a9feb85f53b152aad1c
antoniov <>
2012-06-12 13:35:15 +0000

    update

--- Generator/bin/PHOJET/Phojet.cc
+++ Generator/bin/PHOJET/Phojet.cc
@@ -1,6 +1,6 @@
 /*
- *  $Date: 2012/06/11 15:07:18 $
- *  $Revision: 1.2 $
+ *  $Date: 2012/06/12 13:14:46 $
+ *  $Revision: 1.3.2.1 $
  *  
  */
 
@@ -121,8 +121,11 @@ void Phojet::begin() {
   phoconfig << sRandomSet.str().c_str();
   //phoconfig << (const char*)"ENDINPUT  ";
   phoconfig << "ENDINPUT  ";
-
   phoconfig.rewind();
+
+  // PYDATA initialization 
+  //pydata();
+
   int irej;
   initphojet(iunit,irej);
   /*if(irej != 0) throw edm::Exception(edm::errors::Configuration,"PhojetError") 
