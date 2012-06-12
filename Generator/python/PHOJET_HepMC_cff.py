3f18d2d02d4cf64c46376db9e70bd93bf2123959
antoniov <>
2012-06-12 13:57:23 +0000

    update

--- Generator/python/PHOJET_HepMC_cff.py
+++ Generator/python/PHOJET_HepMC_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 generator = cms.EDProducer("MCFileReader",
-	fileNames = cms.untracked.vstring('file:phojet.hepmc')
+	fileNames = cms.untracked.vstring('phojet.hepmc')
 )
