28d13a81b11c35c15bcfa428643bb47d554d740f
eliza <>
2012-11-22 15:27:09 +0000

    *** empty log message ***

--- Utilities/python/analysisTracks_cfi.py
+++ Utilities/python/analysisTracks_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 analysisTracks = cms.EDProducer("TrackAnalysisSelector",
     src = cms.InputTag("generalTracks"),
-    vertexTag = cms.InputTag("offlinePrimaryVertices"),
+    vertexTag = cms.InputTag("goodOfflinePrimaryVertices"),
     ptMin = cms.double(0.5),
     etaMin = cms.double(-2.4),
     etaMax = cms.double(2.4),
