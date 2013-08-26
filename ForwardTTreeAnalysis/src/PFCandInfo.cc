8daa5f4f22dbaaa3f150ac8f6435ca085b7dd724
eliza <>
2013-08-26 13:10:13 +0000

    *** empty log message ***

--- ForwardTTreeAnalysis/src/PFCandInfo.cc
+++ ForwardTTreeAnalysis/src/PFCandInfo.cc
@@ -49,12 +49,10 @@ void PFCandInfo::fill(PFCandInfoEvent& eventData, const edm::Event& event, const
 
 	// Reset info
 	eventData.reset();
-	//fillPFFlowInfo(eventData,event,setup,PFCandVector);
+	
 	fillPFFlowInfo(eventData,event,setup);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-//void PFCandInfo::pfCandInfo(PFCandInfoEvent& eventData, const edm::Event& event, const edm::EventSetup& setup)
-//void fillPFFlowInfo(PFCandInfoEvent& eventData, const edm::Event& event, const edm::EventSetup& setup, std::vector<PFCand>& PFCandVector){
 void PFCandInfo::fillPFFlowInfo(PFCandInfoEvent& eventData, const edm::Event& event, const edm::EventSetup& setup){
 
 	std::cout << "Using PFCand Info:" << std::endl;
@@ -62,34 +60,17 @@ void PFCandInfo::fillPFFlowInfo(PFCandInfoEvent& eventData, const edm::Event& ev
 	edm::Handle<reco::PFCandidateCollection> particleFlowCollectionH;
 
 	event.getByLabel("particleFlow",particleFlowCollectionH);
-	//reco::PFCandidateCollection const& pflowCollection = *particleFlowCollectionH;
+	
 
 	if( particleFlowCollectionH.isValid() ){
 
-		//PFCandVector.assign(particleFlowCollectionH->size(), PFCand());
-		//Int_t j = 0;
-		//Double_t charge = 0.;
-		/*for(reco::PFCandidateCollection::const_iterator iPFCand_ = particleFlowCollectionH->begin();
-		  iPFCand_ != particleFlowCollectionH->end(); ++iPFCand_, ++j){
-		  PFCandVector[j].SetPxPyPzE(0. , 0. , 0. , 0.);
-		  }*/
 		Int_t i = 0;
 		for(reco::PFCandidateCollection::const_iterator iPFCand = particleFlowCollectionH->begin();
 				iPFCand != particleFlowCollectionH->end();
-				++iPFCand, ++i){ //PFCands += pfCand->p4();
+				++iPFCand, ++i){ 
 
-			/*PFCandVector[i].SetPxPyPzE(iPFCand->px() , iPFCand->py() , iPFCand->pz() , iPFCand->energy() );
-			  std::cout<<"i:"<< i <<"SetPxPyPzE: "<< iPFCand->px() << iPFCand->py() << iPFCand->pz() << iPFCand->energy() <<std::endl;
-			  PFCandVector[i].Charge = iPFCand->charge(); std::cout<<"i:"<< i <<"Charge :"<< PFCandVector[i].Charge <<std::endl;
-			  PFCandVector[i].ParticleId = static_cast<PFCand::ParticleType>(iPFCand->particleId());
-
-			  PFCand pfCandListInfo;
-			  pfCandListInfo.SetPFCandVectorP4(PFCandVector[i].SetPxPyPzE(iPFCand->px() , iPFCand->py() , iPFCand->pz() , iPFCand->energy() ));
-			  pfCandListInfo.SetCharge(PFCandVector[i].Charge);
-			  pfCandListInfo.SetParticleId(PFCandVector[i].ParticleId);*/
 
 			PFCand pfCand;
-			//pfCand.SetPxPyPzE( iPFCand->px() , iPFCand->py() , iPFCand->pz() , iPFCand->energy() );
 			
 			PFCand::LorentzVector p4( iPFCand->px() , iPFCand->py() , iPFCand->pz() , iPFCand->energy() );
 
